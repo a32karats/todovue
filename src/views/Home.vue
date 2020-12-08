ed920c6baa3e1809c7f28fd526bbfe9b3a0bd26e
a32karats <a32karats@yahoo.co.jp>
2020-12-09 00:22:56 +0900

    commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,146 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div class="frame">
+      <h2>Todo List</h2>
+      <div class="basic">
+        <input type="text" v-model="Todo">
+        <button @click="addInput" type="submit">追加</button>
+     </div>
+      <div class="form">
+         <div class="addform" v-for="(text,id) in texts" :key="id">
+          <input type="text" v-model="text[id]">
+          <button class="updatebutton" @click="updateInput(text.id)">更新
+           </button>
+           <button class="removebutton" @click="removeInput(text.id)">削除
+           </button>
+        </div>
+     </div>
+   </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      Todo: "",
+      texts: [],
+    };
   }
-}
+};
 </script>
+
+<style scoped>
+html {
+  position:relative;
+}
+.frame {
+ width:30%;
+ background-color: white;
+ border-radius: 10px;
+ position:absolute;
+ top: 0;
+ right: 0;
+ bottom: 0;
+ left: 0;
+ margin: auto;
+ width: 45%;
+ height: 20%;
+}
+
+h2 {
+  position:absolute;
+  left: 5%;
+  top: 20%;
+  color:black;
+  font-size: 30px;
+}
+
+.basic {
+  margin-top: 80px;
+  margin-left: 35px;
+}
+
+.basic input {
+  height: 40px;
+  width: 75%;
+  border-radius: 8px;
+  color: black;
+  text-align: left;
+  border-color: #CCCCCC;
+  border-style: solid;
+  border-width: 1px;
+}
+
+.basic button {
+  color: #CD70FA;
+  margin-left: 50px;
+  padding: 10px 20px;
+  border-radius: 8px;
+  border-color: #CD70FA;
+  border-style: solid;
+  border-width: 2px;
+  background-color: white;
+  cursor: pointer;
+}
+
+.form {
+  margin-top: 15px;
+  margin-left: 35px;
+}
+
+.addform {
+  margin-bottom: 5px;
+}
+
+.form input {
+  height: 40px;
+  width: 30%;
+  border-radius: 8px;
+  border-color:  #CCCCCC;
+  border-style: solid;
+  border-width: 1px;
+  color: black;
+}
+
+.updatebutton {
+  color: #FA9870;
+  margin-left: 270px;
+  padding: 10px 20px;
+  border-radius: 8px;
+  border-color: #FA9870;
+  border-style: solid;
+  border-width: 2px;
+  background-color: white;
+  cursor: pointer;
+}
+
+.removebutton {
+  color: #70FADC;
+  margin-left: 10px;
+  padding: 10px 20px;
+  border-radius: 8px;
+  border-color: #70FADC;
+  border-style: solid;
+  border-width: 2px;
+  background-color: white;
+  cursor: pointer;
+}
+
+.basic button:hover {
+  background: #CD70FA;
+  color: white;
+}
+
+.updatebutton:hover {
+  background: #FA9870;
+  color: white;
+}
+
+.removebutton:hover {
+  background: #70FADC;
+  color: white
+}
+
+
+</style>
